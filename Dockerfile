a7574bc7a4d271eeaa4a2029922fd0b23385765b
tehdi <tehdi@users.noreply.github.com>
2021-03-31 20:05:56 -0300

    Chapter 2: copy local dictionary file into Docker container so I can use it later

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,6 @@ WORKDIR /usr/src/app
 
 COPY requirements.txt ./
 RUN pip3 install --no-cache-dir -r requirements.txt && rm requirements.txt
+COPY words /usr/share/dict/words
 
 CMD [ "bash" ]
--- build_container.sh
+++ build_container.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+# create "local" copies of files we want copied into the container
+# because... Dockerfile file referencing is weird
+cp /usr/share/dict/british-english ./words
+
 # build (or rebuild) the python-base container using the Dockerfile in the current directory
 docker build -t impractical-base .
+
+# clean up any local temp files we created for the sake of copying into the image
+rm ./words
