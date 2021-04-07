b302331c484c8c7acaa4b52c8d2055df20208e95
tehdi <tehdi@users.noreply.github.com>
2021-04-06 21:40:43 -0300

    Chapter 2: I got tired of all the 2-letter words in my output so I stole a min word length arg from palindromes.py

--- chapter-2_finding-palingram-spells/palingrams.py
+++ chapter-2_finding-palingram-spells/palingrams.py
@@ -12,38 +12,44 @@ parser = argparse.ArgumentParser(description='Find some palingrams.')
 parser.add_argument('-d', '--dictionary', default=DEFAULT_DICTIONARY,
                     help='Path to the file to use as the word dictionary'
                     ' (default: %(default)s)')
+parser.add_argument('-l', '--min_word_length', type=int, default=1,
+                    help='Minimum length of each word in a palingramic pair'
+                    ' for the pair to be included in results'
+                    ' (default: %(default)s)')
 
 
-def find_palingrams(filename):
+def find_palingrams(filename, min_word_length):
     """Print palingrams from a dictionary file."""
     valid_words = set(word for word in load_dictionary.load(filename)
-                   if _is_valid(word))
+                      if _is_valid(word, min_word_length))
 
     palingrams = []
     for word in valid_words:
-        palingramic_pair = _find_palingram(word, valid_words)
+        palingramic_pair = _find_palingram(word, valid_words, min_word_length)
         if palingramic_pair:
-            palingrams.append((palingramic_pair))
+            palingrams.append(palingramic_pair)
     palingrams.sort()
 
-    print("Palingrams found in {}:".format(filename))
+    print("{} palingrams found in {}:".format(len(palingrams), filename))
     # * unpacks args
     # https://docs.python.org/3/tutorial/controlflow.html#unpacking-argument-lists
     print(*palingrams, sep='\n')
 
 
-def _is_valid(word):
+def _is_valid(word, min_word_length):
     return (
-        len(word) > 1
+        len(word) >= min_word_length
         and len(set(word)) > 1
         and "'" not in word
     )
 
 
-def _find_palingram(word, words):
+def _find_palingram(word, words, min_word_length):
     for i in range(len(word)):
         start = word[:i]
         end = word[i:]
+        if len(start) < min_word_length or len(end) < min_word_length:
+            continue
         if _is_palindrome(start) and _is_reversed_word(end, words):
             return (end[::-1], word)
         if _is_palindrome(end) and _is_reversed_word(start, words):
@@ -61,4 +67,4 @@ def _is_palindrome(word):
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    find_palingrams(args.dictionary)
+    find_palingrams(args.dictionary, args.min_word_length)
